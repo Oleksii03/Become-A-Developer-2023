bff707090c9dddcaf5dde7f635af8a6372a950a3
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-07-18 15:59:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const resultStr = prompt('Введіть ваш текст.');
+const resultArr = resultStr.split(' ');
+
+const returnArrLetters = (arrWords) => {
+  const arrLetter = [];
+
+  for (const word of arrWords) {
+
+    const uniqCh = [...word].filter((ch, i, arr) => {
+      return arr.indexOf(ch) === i;
+    });
+
+    const repitCh = [...word].filter((ch, i, arr) => {
+      return arr.indexOf(ch) !== i;
+    });
+
+    for (const ch of uniqCh) {
+      if (!repitCh.includes(ch)) {
+        arrLetter.push(ch);
+        break;
+      }
+    }
+  }
+
+  return arrLetter;
+};
+
+const searchUniqueLetter = (letters) => {
+  const uniqCh = letters.filter((ch, i, arr) => {
+    return arr.indexOf(ch) === i;
+  });
+
+  const repitCh = letters.filter((ch, i, arr) => {
+    return arr.indexOf(ch) !== i;
+  });
+
+  for (const ch of uniqCh) {
+    if (!repitCh.includes(ch)) {
+      return ch;
+    }
+  }
+};
+
+alert(`По вашому запиту знайдено літеру: '${searchUniqueLetter(returnArrLetters(resultArr))}'`);
